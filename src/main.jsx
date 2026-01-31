97a1d81453f714909736e0cbe29fb2772bcbf54c
vendel1123 <vendelhegyi68@gmail.com>
2026-01-31 14:34:20 +0100

    fasza

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-import './index.css'
 import App from './App.jsx'
 import Home from './Home.jsx'
 import Game from './Game.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/app' element={<App/>}/>
       <Route path='/game' element={<Game/>}/>
     </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>,
+
 )
